4204144f25817d1e34a2afea2616a5663532b51b
Ciaran-06 <76187734+Ciaran-06@users.noreply.github.com>
2022-10-14 00:11:17 +0100

    initial commit

---
+++ src/api/openapi.js
@@ -0,0 +1,50 @@
+import swaggerJsDoc from "swagger-jsdoc";
+
+import {
+  loginSchema,
+  registerSchema,
+  changePasswordSchema,
+  userSchema,
+} from "./schemas/auth.js";
+import testModelSchema from "./schemas/testmodel.js";
+
+export const definition = {
+  openapi: "3.0.0.",
+  info: {
+    title: "Demo Project",
+    version: "0.0.1",
+    description: "A REST+JSON API service",
+  },
+  servers: [
+    {
+      url: "/api/v1",
+      description: "API v1",
+    },
+  ],
+  components: {
+    schemas: {
+      loginSchema,
+      registerSchema,
+      changePasswordSchema,
+      User: userSchema,
+      TestModel: testModelSchema,
+    },
+    securitySchemes: {
+      BearerAuth: {
+        type: "http",
+        description: "Simple bearer token",
+        scheme: "bearer",
+        bearerFormat: "simple",
+      },
+    },
+  },
+};
+
+const options = {
+  definition,
+  apis: ["./src/api/routes/*.js"],
+};
+
+const spec = swaggerJsDoc(options);
+
+export default spec;
